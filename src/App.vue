1aea46d117033883d9cea9a8298c83e54408ada8
totoro <arbc139@gmail.com>
2019-10-31 11:25:34 +0900

    First implementation

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <KeyValuePair />
+  </div>
+</template>
+
+<script>
+import KeyValuePair from './components/KeyValuePair.vue'
+
+export default {
+  name: 'app',
+  components: {
+    KeyValuePair
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
